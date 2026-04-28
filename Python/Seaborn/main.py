e594bb9a1b80500096e0e0a5fb0186193067ea51
AurelieDombre <au.dombre@gmail.com>
2026-04-28 10:51:25 +0200

    graphique seaborn

--- Python/Seaborn/main.py
+++ Python/Seaborn/main.py
@@ -1,6 +1,12 @@
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
+# Création d'un DataFrame de démonstration
+data = pd.DataFrame({
+   'age': [23, 45, 56, 78, 21, 34, 49, 50, 20, 31, 73, 24, 58, 46, 35, 62]
+})
 
-sns.scatterplot(x=[1, 2, 3], y=[3, 2, 1])
+# Tracé de l'histogramme avec estimation de la densité
+sns.histplot(data['age'], kde=True)
+plt.title('Distribution des âges')
 plt.show()
