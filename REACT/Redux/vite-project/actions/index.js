0a40cc273d137cea4d7ea105334489c1386f716a
AurelieDombre <au.dombre@gmail.com>
2026-05-11 09:22:03 +0200

    factorisation pour color

--- REACT/Redux/vite-project/actions/index.js
+++ REACT/Redux/vite-project/actions/index.js
@@ -11,6 +11,9 @@ document.querySelector('#decrement').addEventListener('click', () => {
 })
 
 //Lors du clic sur le bouton qui possède l’id “randColor”, nous utilisons la méthode “dispatch” de notre store afin de transmettre une action contenant le type “COLOR_CHANGED”.
+
 document.querySelector('#randColor').addEventListener('click', () => {
-    store.dispatch({ type: 'COLOR_CHANGED' })
+    //Ici nous passons la variable “randomColor” qui contient notre couleur aléatoire dans la propriété “payload” de notre action.
+    let randomColor = `rgb(${Math.random()* 255},${Math.random()* 255},${Math.random()* 255})`
+    store.dispatch({type : 'COLOR_CHANGED',payload : randomColor})
 })
--- REACT/Redux/vite-project/reducers/colorReducer.js
+++ REACT/Redux/vite-project/reducers/colorReducer.js
@@ -2,8 +2,9 @@ const initialState = { color : 'rgb(29,29,29)' }
 export const colorReducer = (state = initialState, action)=>{
     switch(action.type){
         case 'COLOR_CHANGED':
-            let randomColor = `rgb(${Math.random()* 255},${Math.random()* 255},${Math.random()* 255})`
-            return {...state, color : randomColor }
+            //Ici nous retournons un nouvel objet qui contient toutes les propriétés de notre état actuel grâce à l’opérateur de décomposition “...” 
+            // et nous modifions la propriété “color” en lui assignant la valeur de “action.payload” qui correspond à notre couleur aléatoire.
+            return {...state, color : action.payload }
         default : return state
     }
 }
