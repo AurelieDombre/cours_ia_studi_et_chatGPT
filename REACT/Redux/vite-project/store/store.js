80eec88f9b71526edc3e614e485f78473935395b
AurelieDombre <au.dombre@gmail.com>
2026-05-11 10:40:39 +0200

    redux devtool extension

--- REACT/Redux/vite-project/store/store.js
+++ REACT/Redux/vite-project/store/store.js
@@ -2,7 +2,10 @@ import { createStore } from 'redux'
 import { counterReducer } from '../reducers/counterReducer.js'
 import { rootReducer } from '../reducers/rootReducer.js'
 
-export const store = createStore(rootReducer)
+export const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 
 
